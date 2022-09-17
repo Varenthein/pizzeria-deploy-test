d8874053fbdc0a5b2b7f6fceac262a48ef83fe49
Varenthein <raventhein@gmail.com>
2022-09-17 17:11:13 +0200

    fix 4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), 'dist', 'db', 'app.json'));
+const router = jsonServer.router('app.json');
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
